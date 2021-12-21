50338ebcb54de3ecf737d350a0cba14296ce291d
youkim <42.4.youkim@gmail.com>
2021-12-21 18:00:11 +0900

    feat: :triangular_flag_on_post: learn time manipulation

---
+++ philo/includes/philo.h
@@ -0,0 +1,11 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+# include "engine.h"
+
+#endif
---
+++ philo/src/engine/philo.c
@@ -0,0 +1,10 @@
+#include "philo.h"
+
+int main()
+{
+  while (true)
+  {
+    printf("1초마다 문자열을 출력합니다\n");
+    usleep(1000*1000);
+  }
+}
